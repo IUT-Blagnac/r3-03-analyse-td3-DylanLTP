31c91463f78ebb43267c25c43c879d2af530f71d
MaelCarrie <mael.carrie@gmail.com>
2023-09-21 12:26:13 +0200

    commit

---
+++ tp3analyse/src/main/java/com/github/tp3analyse/App.java
@@ -0,0 +1,18 @@
+package com.github.tp3analyse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(hello("4"));
+    }
+
+    public static String hello(String param) {
+        return  param ;
+    }
+    
+}
