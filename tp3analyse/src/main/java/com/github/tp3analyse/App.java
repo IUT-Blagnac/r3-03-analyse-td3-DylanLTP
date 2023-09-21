ebe9dea8e811b0936d7c150bd6cd35c8ff770f14
Dylan Latapie <dylan.ltp@gmail.com>
2023-09-21 12:17:43 +0200

    Méthode helloworld

---
+++ tp3analyse/src/main/java/com/github/tp3analyse/App.java
@@ -0,0 +1,18 @@
+package com.github.tp3analyse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(hello());
+    }
+
+    public static String hello() {
+        return  "Hello World!" ;
+    }
+    
+}
